71d500fb15c525eec2be100bdae6ac17be0b226e
jbaem <101052774+jbaem@users.noreply.github.com>
2023-06-06 17:54:42 +0900

    Create fsku.c

---
+++ fsku.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("Error: the number of argments");
+	}
+	//[fsku] <input_file>
+
+	//[file_name] [I/O command] [size]
+	
+
+	/* Stage1 - Initialization (empty input file) */
+
+	/* Stage2 - Only w commands */
+
+	/* Stage3 - Mix of w and r commands */
+
+	/* Stage4 - Mix of w, r, and d commands */
+	
+	
+	return 0;
+}
