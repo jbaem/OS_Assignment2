a4bd418eb07c7afbacc4aa54928dd1298438a314
jbaem <101052774+jbaem@users.noreply.github.com>
2023-06-08 00:56:23 +0900

    Update fsku.c

--- fsku.c
+++ fsku.c
@@ -203,7 +203,7 @@ void ReadFile(char* file_name, int word_size) {
 		if (word_size <= 0 || curr_fsize <= 0) {
 			return;
 		}
-		printf("%c", *(curr_block + i));
+		printf("%c", curr_block[i]);
 		word_size--;
 		curr_fsize--;
 	}
