6237589067ae06415a33e075f185d5da4d375d9d
jbaem <101052774+jbaem@users.noreply.github.com>
2023-06-08 20:42:54 +0900

    Update fsku.c

--- fsku.c
+++ fsku.c
@@ -372,7 +372,7 @@ int FindDBmap() {
 	int dbmap_index = 0;
 	//base
 	unsigned char* curr_dbmap = (unsigned char*)(data_storage + D_BMAP_BASE);
-	for (int i = 0; i < 8; ++i) {
+	for (int i = 0; i < D_BMAP_BASE / 2; ++i) {
 		unsigned char curr = *(curr_dbmap + i);
 
 		if (curr != 0xFF) {
