3589f13c62828652d2d25273bf96943c70c30051
jbaem <101052774+jbaem@users.noreply.github.com>
2023-06-08 15:48:12 +0900

    Update fsku.c

--- fsku.c
+++ fsku.c
@@ -363,7 +363,7 @@ int FindFile(char* file_name) {
 int FindDBmap() {
 	int dbmap_index = 0;
 	unsigned char* curr_dbmap = data_storage + D_BMAP_BASE;
-	for (int i = 0; i < BLOCK_SIZE / 2; ++i) {
+	for (int i = 0; i < 60; ++i) {
 		unsigned char curr = *(curr_dbmap + i);
 
 		if (curr != 0xFF) {
